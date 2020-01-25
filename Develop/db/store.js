99286264266e59d84713c20773b0dd3143e1c603
Harrison Thomas <hthomas146@utexas.edu>
2020-01-25 12:26:05 -0600

    getNotes now work

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -18,14 +18,48 @@ class Store {
     }
 
     getNotes() {
+        return this.read()
+            .then(notes => {
+                let parseNotes;
+
+                try {
+                    // .concat combines strings together
+                    parseNotes = [].concat(JSON.parse(notes));
+                }
+                catch (err) {
+                    parseNotes = [];
+                }
+                return parseNotes;
+            })
+
+
+        // take the notes from db.json
+        // turn it into a JSON object
+        // add the note to the parseNotes array
+        // return the array
 
     }
 
     addNote(note) {
+        let note = note;
+
+        console.log(note);
+
+        this.getNotes()
+            .then(note => {
+                parseNotes.push(note)
+            })
+
+
+
+        // take the written note
+        // prepend it to the db.json file
+
 
     }
 
     removeNote(id) {
+        // return
 
     }
 }
